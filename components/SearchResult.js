f2660f7f4d386b110544c7ef2886c6aaf61aa19a
euriRepo <eurikamercado03@gmail.com>
2024-05-10 23:41:13 +0800

    b

--- components/SearchResult.js
+++ components/SearchResult.js
@@ -3,9 +3,6 @@ import Parser from 'html-react-parser'
 import PaginationButtons from './PaginationButtons'
 
 export default function SearchResult({ results }) {
-    console.log("1125")
-    console.log(results)
-
     return (
         <div className='w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52'>
             <p className='text-gray-500 text-sm mb-5 mt-5 '>

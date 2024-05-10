59b9785f4ac0cb50ec9c3b5bed2d79e7ace5772a
euriRepo <eurikamercado03@gmail.com>
2024-05-10 22:36:23 +0800

    static key

--- pages/search.js
+++ pages/search.js
@@ -26,9 +26,9 @@ export default function Search({ results }) {
 export async function getServerSideProps(context) {
     const startIndex = context.query.start || "1";
     const mockData = false;
-    //mockData ? response :
+    //mockData ? response :${process.env.GOOGLE_API_KEY}
     const data = await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${context.query.query}${context.query.searchType && "&searchType=image"}&start=${startIndex}`
+        `https://www.googleapis.com/customsearch/v1?key=AIzaSyAt413g_v_7ZlB0IbrufvGrhu6xztqJVwg&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${context.query.query}${context.query.searchType && "&searchType=image"}&start=${startIndex}`
     ).then(response => response.json());
     return {
         props: {

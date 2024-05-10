ad719b8a9ac692e0b2c65ed888b6de36b3161658
euriRepo <eurikamercado03@gmail.com>
2024-05-10 21:57:06 +0800

    update make the mockdata get from the site

--- pages/search.js
+++ pages/search.js
@@ -25,9 +25,8 @@ export default function Search({ results }) {
 
 export async function getServerSideProps(context) {
     const startIndex = context.query.start || "1";
-    console.log("startIndex", startIndex);
     const mockData = false;
-    const data = !mockData ? response : await fetch(
+    const data = mockData ? response : await fetch(
         `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${context.query.query}${context.query.searchType && "&searchType=image"}&start=${startIndex}`
     ).then(response => response.json());
     return {

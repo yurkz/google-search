c5e1a1057db3994faac96d521e9e1c663d8a9df7
euriRepo <eurikamercado03@gmail.com>
2024-05-10 22:50:49 +0800

    test

--- pages/search.js
+++ pages/search.js
@@ -26,9 +26,9 @@ export default function Search({ results }) {
 export async function getServerSideProps(context) {
     const startIndex = context.query.start || "1";
     const mockData = false;
-    //mockData ? response :${process.env.GOOGLE_API_KEY}
+    //mockData ? response :
     const data = await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=AIzaSyAt413g_v_7ZlB0IbrufvGrhu6xztqJVwg&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${context.query.query}${context.query.searchType && "&searchType=image"}&start=${startIndex}`
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.query}${context.query.searchType && "&searchType=image"}&start=${startIndex}`
     ).then(response => response.json());
     return {
         props: {

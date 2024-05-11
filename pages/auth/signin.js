c330bd128922afae47cf5eec6b12832f60a01edf
euriRepo <eurikamercado03@gmail.com>
2024-05-11 12:59:57 +0800

    b

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -3,7 +3,7 @@ import { getProviders, signIn } from 'next-auth/react'
 
 
 export default function signin({ providers }) {
-
+    console.log("providers", providers)
     return (
         <>
             <Header />
